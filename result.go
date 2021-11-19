aaf2e478ba1f8e316379f4300fcdf23bb8d1f0ec
Bob <bob.fang.london@gmail.com>
2021-11-19 04:12:22 +0000

    init

--- result.go
+++ result.go
@@ -2,8 +2,10 @@ package result
 
 type Result[T any] struct {
 	value T
-	err error
+	err   error
 }
+
+// Value related operations
 
 func (r Result[T]) Ok() bool {
 	return r.err == nil
@@ -21,4 +23,24 @@ func (r Result[T]) ValueOrPanic() T {
 		return r.value
 	}
 	panic(r.err)
+}
+
+// Monadic operations
+
+// Map and then are actually the same function, different languages use different names for the same thing
+// Since this is an expeiremental implemenation, I will provide both names so we can deceide which one to use
+// There is some limitation to Golang's Generics at this moment, so we cannot havea a fully fledged Map functon
+// like Haskell.
+// This function has the following type signature in Haskell: (>>=) :: Maybe a -> (a -> Maybe b) -> Maybe b
+// But in Golang, we currently do not support having another idependent type variable in method signature, so we are
+// limited to use T here, but in reality we should have used func(T) Result[U] for type of f.
+func (r Result[T]) Map(f func(T) Result[T]) Result[T] {
+	if r.Ok() {
+		return f(r.value)
+	}
+	return r
+}
+
+func (r Result[T]) Then(f func(T) Result[T]) Result[T] {
+	return r.Map(f)
 }
--- result_test.go
+++ result_test.go
@@ -27,7 +27,7 @@ func TestResultInt(t *testing.T) {
 	if rerr.Ok() {
 		t.Error("Result should not be Ok")
 	}
-	
+
 	if rerr.ValueOr(0) != 0 {
 		t.Error("Result value should be equal to 0")
 	}
@@ -40,5 +40,102 @@ func TestResultInt(t *testing.T) {
 		}
 	}()
 	rerr.ValueOrPanic()
+
+}
+
+func TestResultIntPointer(t *testing.T) {
+	i := 1
+	r := Result[*int]{value: &i}
+
+	if !r.Ok() {
+		t.Error("Result should be Ok")
+	}
+
+	if r.ValueOr(nil) != &i {
+		t.Error("Result value should be equal to i")
+	}
+
+	if r.ValueOrPanic() != &i {
+		t.Error("Result value should be equal to i")
+	}
+}
+
+func TestResultStruct(t *testing.T) {
+	type S struct {
+		A int
+		B string
+	}
+	s := S{A: 1, B: "b"}
+	r := Result[S]{value: s}
+
+	if !r.Ok() {
+		t.Error("Result should be Ok")
+	}
+
+	if r.ValueOr(S{}).A != s.A {
+		t.Error("Result value should be equal to s")
+	}
+
+	if r.ValueOrPanic().A != s.A {
+		t.Error("Result value should be equal to s")
+	}
+
+	err := errors.New("error")
+	rerr := Result[S]{err: err}
+
+	if rerr.Ok() {
+		t.Error("Result should not be Ok")
+	}
+
+	if rerr.ValueOr(S{}).A != 0 {
+		t.Error("Result value should be equal to 0")
+	}
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("Result should panic")
+		} else if !errors.Is(r.(error), err) {
+			t.Error("Result should panic with the error")
+		}
+	}()
+	rerr.ValueOrPanic()
+}
+
+func TestResultStructPointer(t *testing.T) {
+	type S struct {
+		A int
+		B string
+	}
+	s := S{A: 1, B: "b"}
+	r := Result[*S]{value: &s}
+
+	if !r.Ok() {
+		t.Error("Result should be Ok")
+	}
+
+	if r.ValueOr(nil) != &s {
+		t.Error("Result value should be equal to s")
+	}
+
+	if r.ValueOrPanic() != &s {
+		t.Error("Result value should be equal to s")
+	}
+}
+
+func TestResultMonadic(t *testing.T) {
+	i := 1
+	r := Result[int]{value: i}
+
+	r2 := r.Then(func(v int) Result[int] {
+		return Result[int]{value: v + 1}
+	})
+
+	if !r2.Ok() {
+		t.Error("Result should be Ok")
+	}
+
+	if r2.ValueOr(0) != i+1 {
+		t.Error("Result value should be equal to i+1")
+	}
 
 }
